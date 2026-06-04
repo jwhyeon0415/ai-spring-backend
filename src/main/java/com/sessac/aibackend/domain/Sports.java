eb65eb67106e7185bfec3b9515702d0be44716b3
현지원 <jwhyeon0415@gmail.com>
2026-06-04 16:00:56 +0900

    Sports 주석 수정

--- src/main/java/com/sessac/aibackend/domain/Sports.java
+++ src/main/java/com/sessac/aibackend/domain/Sports.java
@@ -9,7 +9,7 @@ import lombok.*;
 @Builder
 public class Sports {
 
-    private Long id;
-    private String sport;
-    private int num;   // 경기 인원 수
+    private Long id;        // 종목 id
+    private String sport;   // 종목 이름
+    private int num;        // 종목 별 인원 수
 }
