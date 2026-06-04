90f9e48ee5d1d88e822398a68a735ec046d81f16
현지원 <jwhyeon0415@gmail.com>
2026-06-04 17:10:57 +0900

    CelebMatcher Bean 객체 추가

--- src/main/java/com/sessac/aibackend/config/AppConfig.java
+++ src/main/java/com/sessac/aibackend/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.sessac.aibackend.config;
 
+import com.sessac.aibackend.util.CelebMatcher;
 import com.sessac.aibackend.util.MessageFormatter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class AppConfig {
     @Bean
     public MessageFormatter messageFormatter() {
         return new MessageFormatter();
+    }
+
+    @Bean
+    public CelebMatcher celebMatcher() {
+        return new CelebMatcher();
     }
 }
